66794c41dbc1a6a4ee2f7ea8d13bda929a2a5536
Sławek <slawekzdunek2.3.6@gmail.com>
2025-10-21 10:45:50 +0200

    Init

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 (function () {
-  const example = document.getElementById('example')
-  const cw1 = document.getElementById('cw1')
-  const cw2 = document.getElementById('cw2')
-  const cw3 = document.getElementById('cw3')
-  const answer = document.getElementById('answer')
+	const example = document.getElementById("example");
+	const cw1 = document.getElementById("cw1");
+	const cw2 = document.getElementById("cw2");
+	const cw3 = document.getElementById("cw3");
+	const answer = document.getElementById("answer");
 
-  example.addEventListener("click", function () {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(array => {
-        console.log(array)
-        answer.innerHTML = JSON.stringify(array);
-      })
-  })
+	example.addEventListener("click", function () {
+		fetch("https://jsonplaceholder.typicode.com/posts")
+			.then((response) => response.json())
+			.then((array) => {
+				console.log(array);
+				answer.innerHTML = JSON.stringify(array);
+			});
+	});
 
-  cw1.addEventListener("click", function () {
-    //TODO
-  })
+	cw1.addEventListener("click", function () {
+		//TODO
+	});
 
-  cw2.addEventListener("click", function () {
-    //TODO
-  })
-
-  cw3.addEventListener("click", function () {
-    //TODO
-  })
-
+	cw2.addEventListener("click", function () {
+		//TODO
+	});
 })();
